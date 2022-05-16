52079a268ac9c12eb039b30581401e32a08aeb69
leejiyoung <103088450+jiyounggo@users.noreply.github.com>
2022-05-16 22:00:39 +0900

    JS

---
+++ main.js
@@ -0,0 +1,121 @@
+'use strict';
+//Navbar
+const navbar = document.querySelector('#navbar');
+
+
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+
+document.addEventListener('scroll',()=>{
+
+if(window.scrollY>navbarHeight){
+    navbar.classList.add('navbar--dark') ;
+}else{
+    navbar.classList.remove('navbar--dark') ;
+}
+
+});
+//scroll
+const navbarMenu= document.querySelector('.navbar__menu');
+
+navbarMenu.addEventListener('click',(event)=>{
+
+ 
+  const link =event. target .dataset.link;
+
+    if(link==null){
+        return;
+    }
+    scrollIntoView(link);
+});
+
+//contact
+
+const contact = document.querySelector('.home__contact');
+
+contact.addEventListener('click',() =>{
+
+    scrollIntoView('#contact');
+});
+
+
+
+
+//scoll slowly transparent
+
+const home= document.querySelector('#home');
+const homeheight = home.getBoundingClientRect().height;
+document.addEventListener('scroll',()=>{
+
+    home.style.opacity=1-window.scrollY/homeheight
+
+});
+
+
+//arrow up
+
+const arrowup=document.querySelector('.arrow-up');
+
+document.addEventListener('scroll', ()=>{
+
+    if(window.scrollY>homeheight/2){
+        arrowup.classList.add('visible');
+    }else{
+        arrowup.classList.remove('visible');
+
+    }
+
+})
+
+//click on the "arrow up"
+
+arrowup.addEventListener('click', ()=>{
+
+    scrollIntoView('#home');
+})
+
+
+
+
+
+//project filtering
+
+const workBtn = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+
+
+workBtn.addEventListener('click',(event)=>{
+const filter = event.target.dataset.filter;
+/* console.log(filter)를 하게되면 span버튼을 클릭했을때 undefined 이 나오기 때문에 
+html에서 각 span data-type 지정해주면 문제가 해결된다*/
+
+//project animation
+projectContainer.classList.add('anim-out');
+
+setTimeout(()=>{
+    projectContainer.classList.remove('anim-out')
+},300);
+//0.3초뒤에 anim-out이 제거됨
+
+
+projects.forEach((pro)=>{
+ 
+if(filter===pro.dataset.type ||filter==='all'){
+    pro.classList.remove('invisible');
+}else{
+    pro.classList.add('invisible');
+}
+
+});
+
+
+
+
+});
+
+function scrollIntoView(selector){
+    const scrollTo=document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior : "smooth"});
+
+}
