1f732f6b2119907c9c85851a20b3120d5450ef8f
leejiyoung <103088450+jiyounggo@users.noreply.github.com>
2022-05-30 21:09:25 +0900

    load

--- main.js
+++ main.js
@@ -130,3 +130,20 @@ function scrollIntoView(selector){
     scrollTo.scrollIntoView({behavior : "smooth"});
 
 }
+
+
+
+// loading
+const loader = document.querySelector('.loader');
+const html = document.querySelector('html');
+
+html.style.overflow = 'hidden'; //로딩 중 스크롤 방지
+
+window.addEventListener('load', ()=>{
+
+     setTimeout(() => { //로딩속도 구현
+         loader.style.opacity = '0';
+         html.style.overflow = 'auto'; //스크롤 방지 해제
+  }, 2500);
+
+})
