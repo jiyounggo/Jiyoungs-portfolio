7460c29f6eeabe2bfd416ffc9758b0850cb6f4e5
leegiyoung <jssa2468@naver.com>
2022-07-20 18:53:07 +0900

    Stuyle : CSS 수정

--- main.js
+++ main.js
@@ -20,17 +20,17 @@ const navbarMenu= document.querySelector('.navbar__menu');
 
 navbarMenu.addEventListener('click',(event)=>{
   const link =event. target .dataset.link;
- 
-  
     if(link==null){
         return;
     }
     scrollIntoView(link);
+  
+   
 });
-
 
 //hamburger button for small screen
 const navbarToggleBtn= document.querySelector('.navbar__toggle-btn');
+
 navbarToggleBtn.addEventListener('click', () => {
 navbarMenu.classList.toggle('open');
 
@@ -132,7 +132,6 @@ function scrollIntoView(selector){
 }
 
 
-
 // loading
 const loader = document.querySelector('.loader');
 const html = document.querySelector('html');
@@ -142,16 +141,66 @@ html.style.overflow = 'hidden'; //로딩 중 스크롤 방지
 window.addEventListener('load', ()=>{
 
      setTimeout(() => { //로딩속도 구현
-    
-        // loader.classList.add('invisible');
          loader.style.opacity = '0';
          html.style.overflow = 'auto'; //스크롤 방지 해제
         
         setTimeout(() => {
-        loader.style.display = 'none';
-          
-      }, 1000);
-        
-  }, 2000);
+         loader.style.display = 'none';
+      }, 800);
+  }, 1000);
+})
 
+//1.모든 섹션 요소들과 해당메뉴아이템들을 가지고 온다
+//2. 인터섹션 옵저버를 이용해 모든 섹션을 관찰한다
+//3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
+
+const sectionIds = [
+    '#home',
+    '#about',
+    '#skills',
+    '#work',
+    '#testimonials',
+    '#contact'
+];
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id => document.querySelector(`[data-link="${id}"]`))
+
+let selectedNavIndex=0;
+let selectedNavItem = navItems[0];
+function selectNavItem (selected){
+    // const navItem = navItems[selectedIndex]
+    // navItem.classList.add('active');
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active');
+}
+const observerOptions = {
+    root:null,
+    rootMargin:'0px',
+    threshold:0.3,
+}
+const observerCallback = (entries,observer) =>{
+    entries.forEach(entry=>{
+        if(!entry.isIntersecting && entry.intersectionRatio>0){
+            const index = sectionIds.indexOf(`#${entry.target.id}`)
+            if(entry.boundingClientRect.y<0){
+                selectedNavIndex = index +1;             
+            }else{
+                selectedNavIndex = index -1;
+            } 
+        }  
+    })
+}
+const observer = new IntersectionObserver(observerCallback,observerOptions);
+sections.forEach(section => observer.observe(section));
+
+window.addEventListener('scroll',()=>{
+    if(window.scrollY ===0 ){
+        selectedNavIndex =0;
+    }else if (
+        window.scrollY + window.innerHeight === 
+        document.body.clientHeight){
+        selectedNavIndex = navItems.length -1;
+    }
+    selectNavItem(navItems[selectedNavIndex]);
 })
